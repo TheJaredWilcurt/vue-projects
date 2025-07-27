04eab79cdce7bae3158ec95faca97276d2c6450c
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-07-27 19:50:06 -0400

    Progress

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # vue-projects
-Vue Projects made by TheJaredWilcurt
+
+Vue Projects made by TheJaredWilcurt.
+
+The idea of this repo is that you will install it like so:
+
+
+```json
+{
+  "dependencies": {
+    "vue-projects": "github:TheJaredWilcurt/vue-projects"
+  }
+}
+```
+
+and then
+
+```js
+import VueProjects from 'vue-projects/VpCards.vue';
+```
+
+```html
+<VueProjects :includeId="true" />
+```
+
+or you can do
+
+```html
+<iframe src="https://thejaredwilcurt.com/vue-projects/#/?style=background:#000;color:#FFF;" />
+```
+
+but then you have to deal with layout stuff on different screen sizes.
--- VpCards.vue
+++ VpCards.vue
@@ -1,31 +1,50 @@
 <template>
-  <div class="vue-projects-cards">
-    <a
-      v-for="project in projects"
-      class="vue-projects-card"
-      :href="project.href"
-      target="_blank"
-    >
-      <img
-        :alt="project.title + ' logo'"
-        class="vue-projects-image"
-        :src="'https://TheJaredWilcurt.com/vue-projects/images/' + project.src"
-      />
-      <span
-        v-html="project.title"
-        class="vue-projects-title"
-      ></span>
-      <span
-        v-html="project.description"
-        class="vue-projects-description"
-      ></span>
-    </a>
+  <div
+    :id="includeId ? 'more-vue-projects' : undefined"
+    class="vue-projects"
+  >
+    <div class="vue-projects-heading">
+      More great Vue libraries!
+      <a
+        v-text="'#'"
+        class="vue-projects-pound"
+        href="#more-vue-projects"
+      ></a>
+    </div>
+    <div class="vue-projects-cards">
+      <a
+        v-for="project in projects"
+        class="vue-projects-card"
+        :href="project.href"
+        target="_blank"
+      >
+        <img
+          :alt="project.title + ' logo'"
+          class="vue-projects-image"
+          :src="'https://TheJaredWilcurt.com/vue-projects/images/' + project.src + cacheBuster"
+        />
+        <span
+          v-html="project.title"
+          class="vue-projects-title"
+        ></span>
+        <span
+          v-html="project.description"
+          class="vue-projects-description"
+        ></span>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'VueProjectsCards',
+  props: {
+    includeId: {
+      type: Boolean,
+      default: true
+    }
+  },
   data: function () {
     return {
       projects: [
@@ -57,11 +76,31 @@ export default {
         // Options API Boilerplate
       ]
     };
+  },
+  computed: {
+    cacheBuster: function () {
+      const now = new Date();
+      return '?cachebust=' + now.getTime();
+    }
   }
 };
 </script>
 
 <style>
+.vue-projects {
+  display: flex;
+  flex-direction: column;
+}
+.vue-projects-heading {
+  margin: 3rem auto;
+  font-size: 2rem;
+  font-style: italic;
+  font-weight: bold;
+  text-align: center;
+}
+.vue-projects-pound {
+  font-size: 0.9em;
+}
 .vue-projects-cards {
   display: flex;
   flex-direction: row;
@@ -83,6 +122,7 @@ export default {
   max-height: 100px;
   background-color: #FFFFFF05;
   border-radius: 8px;
+  margin-bottom: 0.5rem;
   box-shadow: 2px 2px 5px #0006;
   transition: 0.5s ease transform;
 }
@@ -98,5 +138,8 @@ export default {
   font-size: 0.8rem;
   text-align: center;
   opacity: 0.65;
+}
+.vue-projects-card:hover .vue-projects-description {
+  opacity: 1.0;
 }
 </style>
